5cb615ee6f01e35f0ca07a85afa25e3ad90bb11b
MSurai <simonmerkelbach@outlook.de>
2017-02-10 15:44:27 +0100

    Add CsvReader Class
    can parse csv file containing doubles to double[][] using a filename and a split-by-character

---
+++ CsvReader.java
@@ -0,0 +1,40 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.util.ArrayList;
+import java.util.List;
+
+public class CsvReader {
+    public static double[][] loadCsvFile (String fileName, char splitBy) {
+        List<String> csvLines = new ArrayList<>();
+
+        try {
+            BufferedReader br = new BufferedReader(new FileReader(fileName));
+
+            String line;
+            while ((line = br.readLine()) != null) {
+                csvLines.add(line);
+            }
+        }
+        catch (Exception ex) {
+            ex.printStackTrace();
+        }
+
+        int nrOfColumns = 1;
+        for (char c : csvLines.get(0).toCharArray()) {
+            if (c == splitBy) {
+                nrOfColumns++;
+            }
+        }
+
+        double[][] csvValues = new double[csvLines.size()][nrOfColumns];
+        String[] tmp;
+        for (int i = 0; i < csvLines.size(); i++) {
+            tmp = csvLines.get(i).split(String.valueOf(splitBy));
+            for (int j = 0; j < tmp.length; j++) {
+                csvValues[i][j] = Double.parseDouble(tmp[j]);
+            }
+        }
+
+        return csvValues;
+    }
+}
