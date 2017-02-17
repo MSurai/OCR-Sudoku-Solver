5b542891bd88bf763e0c4e0203f0d9be9f701fa4
MSurai <simonmerkelbach@outlook.de>
2017-02-17 07:37:39 +0100

    added option read to double or int array in csvreader
    also tested both, both work fine

--- src/CsvReader.java
+++ src/CsvReader.java
@@ -4,7 +4,41 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class CsvReader {
-    public static double[][] loadCsvFile (String fileName, char splitBy) {
+    public static int[][] loadCsvFileI (String fileName, char splitBy) {
+        List<String> csvLines = new ArrayList<>();
+
+        try {
+            BufferedReader br = new BufferedReader(new FileReader(fileName));
+
+            String line;
+            while ((line = br.readLine()) != null) {
+                csvLines.add(line);
+            }
+        }
+        catch (Exception ex) {
+            ex.printStackTrace();
+        }
+
+        int nrOfColumns = 1;
+        for (char c : csvLines.get(0).toCharArray()) {
+            if (c == splitBy) {
+                nrOfColumns++;
+            }
+        }
+
+        int[][] csvValues = new int[csvLines.size()][nrOfColumns];
+        String[] tmp;
+        for (int i = 0; i < csvLines.size(); i++) {
+            tmp = csvLines.get(i).split(String.valueOf(splitBy));
+            for (int j = 0; j < tmp.length; j++) {
+                csvValues[i][j] = Integer.parseInt(tmp[j]);
+            }
+        }
+
+        return csvValues;
+    }
+
+    public static double[][] loadCsvFileD (String fileName, char splitBy) {
         List<String> csvLines = new ArrayList<>();
 
         try {
