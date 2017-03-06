1ecda12033130aa793cd45a17c2c9f3b72b68bf9
MSurai <simonmerkelbach@outlook.de>
2017-03-06 20:57:28 +0100

    refactoring

--- src/CsvReader.java
+++ src/CsvReader.java
@@ -4,38 +4,26 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class CsvReader {
+    public static int[][] loadCsvFileI (String fileName) {
+        return loadCsvFileI(fileName, ',');
+    }
+
+    // get csvValues from double method, typecast to int
     public static int[][] loadCsvFileI (String fileName, char splitBy) {
-        List<String> csvLines = new ArrayList<>();
+        double[][] csvValues = loadCsvFileD(fileName, splitBy);
+        int[][] tmp = new int[csvValues.length][csvValues[0].length];
 
-        try {
-            BufferedReader br = new BufferedReader(new FileReader(fileName));
-
-            String line;
-            while ((line = br.readLine()) != null) {
-                csvLines.add(line);
-            }
-        }
-        catch (Exception ex) {
-            ex.printStackTrace();
-        }
-
-        int nrOfColumns = 1;
-        for (char c : csvLines.get(0).toCharArray()) {
-            if (c == splitBy) {
-                nrOfColumns++;
+        for (int i = 0; i < tmp.length; i++) {
+            for (int j = 0; j < tmp[i].length; j++) {
+                tmp[i][j] = (int) csvValues[i][j];
             }
         }
 
-        int[][] csvValues = new int[csvLines.size()][nrOfColumns];
-        String[] tmp;
-        for (int i = 0; i < csvLines.size(); i++) {
-            tmp = csvLines.get(i).split(String.valueOf(splitBy));
-            for (int j = 0; j < tmp.length; j++) {
-                csvValues[i][j] = Integer.parseInt(tmp[j]);
-            }
-        }
+        return tmp;
+    }
 
-        return csvValues;
+    public static double[][] loadCsvFileD (String fileName) {
+        return loadCsvFileD(fileName, ',');
     }
 
     public static double[][] loadCsvFileD (String fileName, char splitBy) {
